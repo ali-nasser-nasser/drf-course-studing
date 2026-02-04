eb11c73b7c37b80cc068caf552b5a589bf64ed1e
ali-nass <ali.slman7.98@gmail.com>
2026-02-04 23:25:41 +0300

    we give a permission auth then we wrote some tests

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     # path('products/<int:pk>/', views.ProductDetail.as_view()),
     path('products/<int:product_id>/', views.ProductDetail.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
-    path('user-orders/', views.UserOrderListAPIView.as_view()),
+    path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
 ]
