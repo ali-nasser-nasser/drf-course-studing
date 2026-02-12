3f9ae3e61277dce0160cec5494f917869967ccc9
ali-nass <ali.slman7.98@gmail.com>
2026-02-12 22:18:19 +0300

    filter-search-ordering

--- api/views.py
+++ api/views.py
@@ -15,6 +15,9 @@ from rest_framework.permissions import (
 )
 from rest_framework.views import APIView
 from api.filters import ProductFilter
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -22,6 +25,13 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     # filterset_fields= ('name', 'price')
     filterset_class= ProductFilter
+    filter_backends= [
+        DjangoFilterBackend, 
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    search_fields= ['name', 'description']
+    ordering_fields= ['name', 'price', 'stock']
     
     def get_permissions(self):
         self.permission_classes= [AllowAny] 
